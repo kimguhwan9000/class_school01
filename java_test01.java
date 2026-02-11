6eff24c040322c84682f4761a0779b4809fca78e
Kimguhwan9000 <kpax816983@gmail.com>
2026-02-11 10:54:20 +0900

    kkkk

--- java_test01.java
+++ java_test01.java
@@ -1,5 +1,11 @@
 public class java_test01 {
     public static void main(String[] args) {
         System.out.println("성우 특집으로 꾸며 드리고 있는 KBS연기 대상");
+
+        // 1. 변수 선언과 값 넣기
+        String title = "KBS연기대상";
+
+        // 2. 변수 사용해서 출력하기
+        System.out.println(title);
     }
 }
