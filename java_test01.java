4bac14eeef1ece3286ee570777d313fdfee065e3
Kimguhwan9000 <kpax816983@gmail.com>
2026-02-11 11:25:41 +0900

    java class shool 20260211 1126 okok

--- java_test01.java
+++ java_test01.java
@@ -4,8 +4,38 @@ public class java_test01 {
 
         // 1. 변수 선언과 값 넣기
         String title = "KBS연기대상";
+        int year = 2026;
 
         // 2. 변수 사용해서 출력하기
-        System.out.println(title);
+        System.out.println(year + "년" + title);
+
+        sayHello();
+
+        // [중요] 다른 클래스 사용하기
+        // 클래스이름 변수이름 = new 클래스이름();
+        Voice_actor Actor = new Voice_actor();
+
+
+        //다른 클래스 안의 함수 사용하기
+        Actor.actors();
+
+        //다른 클래스 안의 함수의 변수의 값 바꾸는 방법
+        Actor.name = "안지환";
+
+        Actor.actors();
+        
+    }
+
+    public static void sayHello(){
+        System.out.println("반갑습니다 저는 오랜만에 자바 공부중입니다.");
+    }
+}
+
+// 새로운 클래스 만들기
+class Voice_actor {
+    String name = "강수진";
+    
+    void actors(){
+        System.out.println(name);
     }
 }
